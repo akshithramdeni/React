1fa00cb2b34dd8512f1295de319659b2648a2891
Ramdeni Akshith <117385626+ramdeniakki@users.noreply.github.com>
2022-12-09 21:10:07 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var react = require("react");
+var reactDom = require("react-dom");
+
+// reacrDom.render(what to show,where to  show)
+
+reactDom.render(<h2>hello people</h2>, document.getElementById("root"));
